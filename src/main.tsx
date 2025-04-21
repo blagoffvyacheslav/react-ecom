841c662ed653f76b275b6cc62eec2a7c86bceda6
Blagov, Vyacheslav <blagoff@me.com>
2025-04-21 23:21:37 +0200

    styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
+import '@styles/global.scss';
 import App from '@App/App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
